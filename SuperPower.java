5049b9f10783781bcbe25b41340f55d23e0a2379
Dr-MarcusI <oshomarcus20@gmail.com>
2022-09-07 14:43:42 +0100

    Programming Assigment Unit 1

---
+++ SuperPower.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class SuperPower {
+
+	public static void main(String[] args) {
+		String power = JOptionPane.showInputDialog("What is your super power?");
+		
+		
+		power = power.toUpperCase();
+		
+		
+	 JOptionPane.showMessageDialog(null,power + " SUPER POWERS TO THE RESCUE!");
+
+	}
+
+}
